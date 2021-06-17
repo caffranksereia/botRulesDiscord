b39da725d68b3b09e6bed2d9083a1da17b2d2282
ferafael <circuncisaofabio@icloud.com>
2021-06-17 15:59:10 -0300

    config

--- bot-env/main.py
+++ bot-env/main.py
@@ -1,3 +1,14 @@
 import json
 import discord
+from discord.ext import commands
+
+
+with open ('config.json') as e:
+    infos = json.load(e)
+    TOKEN = infos['token']
+    prefix = infos['prefix'] 
+
+client = commands.Bot(command_prefix = prefix ,intents = discord.Intets.all())
+
+client.run(TOKEN)
 
